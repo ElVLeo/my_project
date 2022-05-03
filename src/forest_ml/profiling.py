625ab04ef261cb69fdf1c658b3585f541cc4606c
Elena Burlo <burlolena@gmail.com>
2022-05-04 02:44:55 +0300

    Add EDA

---
+++ src/forest_ml/profiling.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from pandas_profiling import ProfileReport
+df = pd.read_csv('data/train.csv')
+def profile():
+    eda=ProfileReport(df, title="Pandas Profiling Report")
+    eda.to_file(output_file="Pandas_profiling.html")
