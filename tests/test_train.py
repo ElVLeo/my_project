5d961da591a72163e125847e7f03b7e7ccbfdb76
Elena Burlo <burlolena@gmail.com>
2022-05-11 10:26:20 +0300

    Add test for criterion

---
+++ tests/test_train.py
@@ -0,0 +1,28 @@
+from click.testing import CliRunner
+import pytest
+
+from forest_ml.train import train
+
+
+@pytest.fixture
+def runner() -> CliRunner:
+    """Fixture providing click runner."""
+    return CliRunner()
+
+
+def test_error_for_criterion(
+    runner: CliRunner
+) -> None:
+    """It fails when criterion is not real."""
+    result = runner.invoke(
+        train,
+        [
+            "--criterion",
+            'gin',
+        ],
+    )
+    assert result.exit_code == 2
+    assert "Invalid value for '--criterion'" in result.output
+
+
+
